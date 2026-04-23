78bc23a8aa7de515f3495d41cc4a04ce07bb1214
null2700 <sohamgurav.skn.comp@gmail.com>
2026-04-23 23:12:41 +0530

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Text, ForeignKey, Float, DateTime
 from datetime import datetime
 from sqlalchemy.orm import relationship
-from database import Base
+from backend.database import Base
 
 class User(Base):
     __tablename__ = "users"
